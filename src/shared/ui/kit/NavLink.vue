54a6ad41a17ad527a6f477a802d96e053be744aa
tryproxy <tryproxy@protonmail.com>
2026-01-27 21:49:14 +0400

    feat(UI): add NavLink

---
+++ src/shared/ui/kit/NavLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <RouterLink :to="to" class="text-lg" exact-active-class="underline" v-bind="$attrs">
+    <slot />
+  </RouterLink>
+</template>
